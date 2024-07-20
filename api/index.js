31fdb937fb28d187b8d23c9099d6a3fa8a88550b
abdulmoizsheraz <moiz77131@gmail.com>
2024-07-20 22:03:53 +0500

    fix

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const IncomeStatement = require("../Model/IncomeStatementModel")
+const User = require("../Model/userModel");
+const bcrypt = require("bcryptjs");
+const generateTokenAndSetCookies = require("../utils/generateToken");
 
 // Load environment variables
 dotenv.config();
@@ -15,29 +17,58 @@ const app = express();
 const corsOptions = {
   origin: "https://bread-n-butter-client.vercel.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  optionsSuccessStatus: 204,
 };
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
 // Import routes
-const user = require("../routes/userRoutes");
-const quarter = require("../routes/quarterRoutes");
-const income = require("../routes/IncomeRoutes");
-const admin = require("../routes/adminRoute");
-const quarter2 = require("../routes/quarter2Route");
+// const user = require("../routes/userRoutes");
+// const quarter = require("../routes/quarterRoutes");
+// const income = require("../routes/IncomeRoutes");
+// const admin = require("../routes/adminRoute");
+// const quarter2 = require("../routes/quarter2Route");
 
 // Route handlers
 app.get("/health", (req, res) => {
   res.status(200).json("Health checking");
 });
+app.post("/api/v1/login",async(req, res) => {
+  try {
+    const { email, password } = req.body;
 
-app.use("/api/v1", user);
-app.use("/api/v1", quarter);
-app.use("/api/v1/quarter2", quarter2);
-app.use("/api/v1", income);
-app.use("/api/v1", admin);
+    const user = await User.findOne({ email });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({
+        error: "Invalid credentials",
+      });
+    }
+
+    const token = generateTokenAndSetCookies(user._id, res);
+    console.log(token);
+    res.status(201).json({
+      data: user,
+      token,
+      message: "login successful",
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+})
+
+// app.use("/api/v1", user);
+// app.use("/api/v1", quarter);
+// app.use("/api/v1/quarter2", quarter2);
+// app.use("/api/v1", income);
+// app.use("/api/v1", admin);
 
 
 
