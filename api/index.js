74d1e70e2c36be3b4541ab83041c8306372a61c8
abdulmoizsheraz <moiz77131@gmail.com>
2024-07-20 22:23:29 +0500

    fix

--- api/index.js
+++ api/index.js
@@ -1,38 +1,66 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const user = require("../routes/userRoutes");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const IncomeStatement = require("../Model/IncomeStatementModel")
+
+// Load environment variables
+dotenv.config();
+
+// Initialize Express app
 const app = express();
-require('dotenv').config();
 
+// Middleware
+const corsOptions = {
+  origin: "https://bread-n-butter-client.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(cors());
 
-mongoose
-.connect("mongodb+srv://sherazmoiz9:FMGxSK0XXAOR42S0@cluster0.l6qfsrz.mongodb.net/buygold", {
-})
-.then((data) => {
-  console.log(`Mongodb connected with server: ${data.connection.host}`);
+// Import routes
+const user = require("../routes/userRoutes");
+const quarter = require("../routes/quarterRoutes");
+const income = require("../routes/IncomeRoutes");
+const admin = require("../routes/adminRoute");
+const quarter2 = require("../routes/quarter2Route");
+
+// Route handlers
+app.get("/health", (req, res) => {
+  res.status(200).json("Health checking");
 });
 
-
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-
-// User signup endpoint
 app.use("/api/v1", user);
+app.use("/api/v1", quarter);
+app.use("/api/v1/quarter2", quarter2);
+app.use("/api/v1", income);
+app.use("/api/v1", admin);
 
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+
+
+// Connect to MongoDB
+const DB = process.env.DB_URL.replace("<PASSWORD>", process.env.DB_PASSWORD);
+
+const connectToMongodb = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error in connecting to MongoDB", error.message);
+  }
+};
+
+// Start server and connect to MongoDB
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+  connectToMongodb();
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-app.get('/', (req, res) => {
-  res.send('Server is running..');
-})
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 
 module.exports = app;
