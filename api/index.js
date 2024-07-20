5b1d846e27951be7ead38f743b021965c822ee50
abdulmoizsheraz <moiz77131@gmail.com>
2024-07-20 23:27:19 +0500

    fix

--- api/index.js
+++ api/index.js
@@ -1,44 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-// Load environment variables
+
 dotenv.config();
 
-// Initialize Express app
 const app = express();
 
-// Middleware
-const corsOptions = {
-  origin: "http://localhost:3000",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  optionsSuccessStatus: 204,
-};
-app.use(cors(corsOptions));
-app.use(cookieParser());
-app.use(express.json());
-
-// Import routes
-const user = require("../routes/userRoutes");
-const quarter = require("../routes/quarterRoutes");
-const income = require("../routes/IncomeRoutes");
-const admin = require("../routes/adminRoute");
-const quarter2 = require("../routes/quarter2Route");
-
-// Route handlers
-app.get("/health", (req, res) => {
-  res.status(200).json("Health checking");
-});
-
-app.use("/api/v1", user);
-app.use("/api/v1", quarter);
-app.use("/api/v1/quarter2", quarter2);
-app.use("/api/v1", income);
-app.use("/api/v1", admin);
-
-// Connect to MongoDB
 const DB = process.env.DB_URL.replace("<PASSWORD>", process.env.DB_PASSWORD);
 
 const connectToMongodb = async () => {
@@ -46,12 +16,41 @@ const connectToMongodb = async () => {
     await mongoose.connect(DB);
     console.log("Connected to MongoDB");
   } catch (error) {
-    console.log("Error in connecting to MongoDB", error.message);
+    console.log("Error in connecting to Mongodb", error.message);
   }
 };
 
-// Start server and connect to MongoDB
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+
+// Route imports
+const user = require("../routes/userRoutes");
+const quarter = require("../routes/quarterRoutes");
+const income = require("../routes/IncomeRoutes");
+const admin = require("../routes/adminRoute");
+const quarter2 = require("../routes/quarter2Route");
+
+// Health check endpoint
+app.get("/health", (req, res) => {
+  res.status(200).json("Health checking");
+});
+
+// API routes
+app.use("/api/v1", user);
+app.use("/api/v1", quarter);
+app.use("/api/v1/quarter2", quarter2);
+app.use("/api/v1", income);
+app.use("/api/v1", admin);
+
 const PORT = process.env.PORT || 7000;
+
 app.listen(PORT, () => {
   connectToMongodb();
   console.log(`Server is running on http://localhost:${PORT}`);
--- middleware/auth.js
+++ middleware/auth.js
@@ -35,6 +35,7 @@ exports.authorizedRole = (...roles) => {
         message: `${req.user.role} is not authorized to perform this action`,
       });
     }
+
     next();
   };
 };
